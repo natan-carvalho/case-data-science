27733b0400f55f3c54c83d62263c90e0fbc4c065
natan-carvalho <natan.carvalho123@hotmail.com>
2026-02-20 23:16:46 -0300

    :ok_hand: add: added script for download database

---
+++ cmd.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ./data
+curl -o ./data/articles.csv https://download1980.mediafire.com/dheq20iyrijglpmHbs8Mv5BQIDWKpsLOMAseTKrIh6s3HW2ugPKhcEIL-bMC7sB3b5xE-nhJ5LE8b27UdrrL-0k6SrMoH-S1EYhXVrdtdjKwTvewRTakE0XF556l-qklG_G9D0B2ZgrUO8_KngSm_BqV1Dtqfn7nbTzKlK1pik0/nfy0816rh9thmy4/articles.csv
